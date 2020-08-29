37135591b8cf049a2e858ad8c0fc8f33607c6151
Moshenagar <mosesnagar@gmail.com>
2020-08-29 23:52:44 +0300

    Adding new model controller

---
+++ tutor/controllers/model.py
@@ -0,0 +1,17 @@
+from ..models import Model
+from .. import db
+from flask import jsonify
+
+
+class ModelController:
+    def getAllModels():
+        models = Model.query.all()
+        return jsonify([e.serialize() for e in models])
+
+    def createNewModel(modelName):
+        model = Model(name=modelName)
+        db.session.add(model)
+        db.session.commit()
+        return jsonify({'id' : model.id, 'status' : 200})
+
+
--- tutor/routing.py
+++ tutor/routing.py
@@ -1,7 +1,5 @@
-from flask import jsonify
-from tutor import app, db
-from tutor.controllers import hello
-from tutor.models import Model
+from tutor import app
+from tutor.controllers import hello, model
 
 
 #hello world
@@ -13,15 +11,12 @@ def hello_route():
 # example to get all models
 @app.route('/models')
 def models_route():
-    books = Model.query.all()
-    return jsonify([e.serialize() for e in books])
+    return model.ModelController.getAllModels()
 
 
 # example of a route with different methods
 # example to return a json 
 @app.route('/model/store', methods=['GET', 'POST'])
 def store_book_route():
-    model = Model(name="moshe")
-    db.session.add(model)
-    db.session.commit()
-    return jsonify({'id' : model.id, 'status' : 200})
+    return model.ModelController.createNewModel(modelName='model')
+
