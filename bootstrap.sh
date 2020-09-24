e7bc98c59f7b51e7c02cf4989e45a42d40bb908d
Moshe Nagar <mosesnagar@gmail.com>
2020-09-24 12:34:00 +0300

    Change bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,9 +8,15 @@ sudo apt install -y python3.7 python3-pip python3.7-dev postgresql postgresql-co
 
 echo "installing dependencies"
 pip3 install pipenv
-export PIPENV_VENV_IN_PROJECT="enabled"
-export VIRTUALENV_ALWAYS_COPY=1
-cd /vagrant/ && pipenv sync --dev
+
+if [ ! "$CI" ] 
+then
+  export PIPENV_VENV_IN_PROJECT="enabled"
+  export VIRTUALENV_ALWAYS_COPY=1
+  cd /vagrant
+fi
+
+pipenv sync --dev
 
 
 echo "initializing new DB"
@@ -23,7 +29,12 @@ export FLASK_APP=/vagrant/run.py
 export FLASK_ENV=development    
 
 echo "initializing DB and migrations"
-cd /vagrant
+
+if [ ! "$CI" ] 
+then
+  cd /vagrant
+fi
+
 pipenv run python3 run.py db init
 pipenv run python3 run.py db migrate
 pipenv run python3 run.py db upgrade
