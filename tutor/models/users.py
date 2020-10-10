76edeec64219de1f0b7ab23832065ff96ecd3897
Moshenagar <mosesnagar@gmail.com>
2020-10-10 21:55:18 +0300

    Add favorites table, and update user model to handle favorites courses

--- tutor/models/users.py
+++ tutor/models/users.py
@@ -2,9 +2,20 @@ from .. import db, bcrypt, login_manager
 from flask_login import UserMixin
 
 
+if __name__ == "__main__":
+    from ..models.course import Course
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return Users.query.get(int(user_id))
+
+
+# Add favorites tables, to handle many-to-many relationship
+favorites = db.Table('favorites',
+                          db.Column('user_id', db.Integer,
+                                    db.ForeignKey('users.id')),
+                          db.Column('course_id', db.Integer, db.ForeignKey('course.id')))
 
 
 class Users(db.Model, UserMixin):
@@ -14,6 +25,20 @@ class Users(db.Model, UserMixin):
     username = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(60), nullable=False)
+    
+    favorites = db.relationship("Course", secondary=favorites)
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def addFavorite(self, course):
+        self.favorites.append(course)
+        db.session.commit()
+        
+    def removeAllFavorites(self):
+        self.favorites = []
+        db.session.commit()
 
     def hash_pass(plaintext):
         return bcrypt.generate_password_hash(plaintext).decode('utf-8')
